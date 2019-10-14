14213a3bb40b8b0bdeb74f1e84ce073d1b9887ad
tinurohan <tinu.mw@gmail.com>
2019-10-14 10:17:07 -0500

    Initial commit

---
+++ src/test/java/seleniumdemo/LoginTest.java
@@ -0,0 +1,42 @@
+package seleniumdemo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		driver = new ChromeDriver();	
+		
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://gmail.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.id("identifierId")).sendKeys("tinu.mw@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
+		driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("ABD");
+		
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		
+		driver.quit();
+	}
+	
+
+}
