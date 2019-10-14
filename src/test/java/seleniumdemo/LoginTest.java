7acc1117c5ba4cb56e0e5e02ce22d24cb5b44a67
tinurohan <tinu.mw@gmail.com>
2019-10-14 11:20:47 -0500

    Committing changes made to the initial commit

--- src/test/java/seleniumdemo/LoginTest.java
+++ src/test/java/seleniumdemo/LoginTest.java
@@ -5,6 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -28,7 +30,9 @@ public class LoginTest {
 		driver.findElement(By.id("identifierId")).sendKeys("tinu.mw@gmail.com");
 		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
 		driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("ABD");
-		
+		//driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/span/span")).click();
+
+			
 	}
 	
 	@AfterSuite
